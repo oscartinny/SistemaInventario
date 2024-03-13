892c8c09719b54405d57a127d69b18428b9865c0
oscarMedina <oscar.medina.olvera@gmail.com>
2024-03-12 23:37:19 -0600

    Configuracion del Proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(60,ErrorMessage ="Nombre no debe ser Maximo de 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La descripció es requerido")]
+        [MaxLength(1000, ErrorMessage = "Descripción no debe ser Maximo de 100 caracteres")]
+        public string Descripcion { get; set;}
+
+        [Required(ErrorMessage = "El estado es requerido")]
+        public bool Estado { get; set;}
+    }
+}
