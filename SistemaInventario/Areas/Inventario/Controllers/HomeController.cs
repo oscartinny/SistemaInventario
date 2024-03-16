04e36930a26612a1d1ba2bad212ace57fdd155a9
oscarMedina <oscar.medina.olvera@gmail.com>
2024-03-16 09:09:22 -0600

    Diseño Inicio

--- SistemaInventario/Areas/Inventario/Controllers/HomeController.cs
+++ SistemaInventario/Areas/Inventario/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.DAO.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
 using SistemaInventario.Modelos.ViewModels;
 using System.Diagnostics;
 
@@ -8,15 +10,19 @@ namespace SistemaInventario.Areas.Inventario.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnidadTrabajo _unidadTrabajo;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnidadTrabajo unidadTrabajo)
         {
             _logger = logger;
+            _unidadTrabajo = unidadTrabajo;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            //Obtenemos todo el listado de productos para ser enviados a la vista
+            IEnumerable<Producto> produtoLista = await _unidadTrabajo.Producto.obtenerTodos();
+            return View(produtoLista);
         }
 
         public IActionResult Privacy()
